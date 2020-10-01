b3f0ee9b09a1dbcc17934d23cf1fec8eb3866ca2
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:41:10 +0300

    Add project files.

---
+++ ContextMenu2/ContextMenuForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ContextMenu2
+{
+    public partial class ContextMenuForm : Form
+    {
+        private const int SW_SHOWNOACTIVATE = 4;
+        private const int HWND_TOPMOST = -1;
+        private const uint SWP_NOACTIVATE = 0x0010;
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        static extern bool SetWindowPos(
+             int hWnd,             // Window handle
+             int hWndInsertAfter,  // Placement-order handle
+             int X,                // Horizontal position
+             int Y,                // Vertical position
+             int cx,               // Width
+             int cy,               // Height
+             uint uFlags);         // Window positioning flags
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static void ShowInactiveTopmost(Form frm)
+        {
+            ShowWindow(frm.Handle, SW_SHOWNOACTIVATE);
+            SetWindowPos(frm.Handle.ToInt32(), HWND_TOPMOST,
+            frm.Left, frm.Top, frm.Width, frm.Height,
+            SWP_NOACTIVATE);
+        }
+
+        public ContextMenuForm()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+            this.MouseClick += ContextMenuForm_MouseClick;
+            this.LostFocus += ContextMenuForm_LostFocus;
+        }
+
+        private void ContextMenuForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ContextMenuForm_LostFocus(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
